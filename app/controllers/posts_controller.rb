e1722607665920ea93f81af3dff0d976519ffe4c
SanSanich3D <sansanich3d@gmail.com>
2016-07-19 23:10:15 +0300

    Associating Users with Posts and Comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 	
 	before_filter :authenticate_user!, only: [:create, :upvote]
-	
+
 	respond_to :json
 
 	def index
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		respond_with Post.create(post_params)
+		respond_with Post.create(post_params.merge(user_id: current_user.id))
 	end
 
 	def show
